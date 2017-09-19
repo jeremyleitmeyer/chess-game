30b7c69533f2e5542e80059814a707a3666a26a2
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-09-19 11:52:56 -0400

    add css

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var app = express();
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/chess-game'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules')); 
+
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
 });
